88cce65345ed4f5a312e15a2b25aac6395af45c1
Neeru Rathour <Neerurathour@gmail.com>
2024-06-25 11:25:24 +0000

    new text added to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,8 @@
-console.log("Text")
-
-console.log("Hello everyone")
-
-
+console.log("Text");
+console.log("Text - branch A");
+console.log("Text - branch A2");
+console.log("Hello everyone");
 console.log("text-olu-branch")
-
-
-
 console.log("hello from Sasha");
-
-
-console.log("Neeru here");
+console.log("SteveT Making a second change")
+console.log("second hello");console.log("Neeru here");
